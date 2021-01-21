bfeffb3791f3fd34e66794e9ccf85133c035b5d8
Daniel Kaiser <Daniel.Kaiser@empalis.com>
2021-01-21 14:38:41 +0100

    test database

--- webroot/index.php
+++ webroot/index.php
@@ -1,40 +1,129 @@
+<!DOCTYPE html> 
+<html> 
+<head>
+  <meta charset="utf-8">
+  <title>Gästebuch</title> 
+</head> 
+<body>
+ 
 <?php
-/**
- * The Front Controller for handling every request
- *
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.2.9
- * @license       MIT License (https://opensource.org/licenses/mit-license.php)
- */
-
-// Check platform requirements
-require dirname(__DIR__) . '/config/requirements.php';
-
-// For built-in server
-if (php_sapi_name() === 'cli-server') {
-    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
-
-    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
-    $file = __DIR__ . $url['path'];
-    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
-        return false;
-    }
+$pdo = new PDO('mysql:host=mysql:3306;dbname=empalis', 'cakephp', 'pass4root');
+$show_form = true; 
+$error = null;
+ 
+//Das Formular wurde abgesendet, überprüfe den Inhalt und speichere es ab
+if(isset($_GET['submit'])) {
+ $name = trim($_POST['name']);
+ $email = trim($_POST['email']);
+ $text = trim($_POST['text']);
+ 
+ //Überprüfen dass die E-Mail-Adresse gültig ist
+ if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+ $error = 'Bitte eine gültige E-Mail-Adresse eingeben<br>';
+ } else if(empty($name)) {
+ $error = 'Bitte einen Namen eingeben<br>';
+ } else if(empty($text)) {
+ $error = 'Bitte einen Text eingeben<br>'; 
+ } else {
+ $statement = $pdo->prepare("INSERT INTO gaestebuch (name, email, text) VALUES (:name, :email, :text)");
+ $result = $statement->execute(array('name' => $name, 'email' => $email, 'text' => $text));
+ 
+ if($result) {
+ echo '<b>Dein Eintrag wurde erfolgreich gespeichert</b><br><br>';
+ $show_form = false;
+ } else {
+ $error = 'Beim Abspeichern ist leider ein Fehler aufgetreten<br>';
+ }
+ }
 }
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-use App\Application;
-use Cake\Http\Server;
-
-// Bind your application to the server.
-$server = new Server(new Application(dirname(__DIR__) . '/config'));
-
-// Run the request/response through the application and emit the response.
-$server->emit($server->run());
+?>
+ 
+<?php 
+if(!is_null($error)) { //Ein Fehler ist aufgetreten
+ echo $error;
+}
+ 
+//Ausgabe des Formulars nur wenn $showForm == true ist
+if($show_form): 
+?>
+ <form action="?submit=1" method="post">
+ Name:<br>
+ <input type="text" size="40" maxlength="250" name="name"><br><br>
+ 
+ E-Mail:<br>
+ <input type="email" size="40" maxlength="250" name="email"><br><br>
+ 
+ Text:<br>
+ <textarea cols="50" rows="9" name="text"></textarea><br><br>
+ 
+ <input type="submit" value="Eintragen">
+ </form> 
+<?php 
+endif;
+?>
+ 
+<hr>
+ 
+<?php 
+/***********************
+ * Ausgabe der Einträge
+ ***********************/
+ 
+//Ermittelt die Anzahl der Beiträge
+$statement = $pdo->prepare("SELECT COUNT(*) AS anzahl FROM gaestebuch WHERE deleted_at IS NULL");
+$statement->execute();
+$row = $statement->fetch();
+$anzahl_eintrage = $row['anzahl'];
+echo "$anzahl_eintrage Personen sind eingetragen<br><br>";
+ 
+ 
+//Berechne alles notwendige für die Blätterfunktion
+$seite = 1;
+if(isset($_GET['seite'])) {
+ $seite = intval($_GET['seite']);
+}
+ 
+$beitraege_pro_seite = 20;
+$start = ($seite-1)*$beitraege_pro_seite;
+ 
+//Abfrage der Datenbank und Ausgabe der Daten
+$statement = $pdo->prepare("SELECT * FROM gaestebuch WHERE deleted_at IS NULL ORDER BY id DESC LIMIT :start, :limit");
+$statement->bindParam(':start', $start, PDO::PARAM_INT);
+$statement->bindParam(':limit', $beitraege_pro_seite, PDO::PARAM_INT);
+$statement->execute();
+while($row = $statement->fetch()) {
+ $name = htmlentities($row['name']);
+ $email = htmlentities($row['email']);
+ $text = nl2br(htmlentities($row['text']));
+ $date = new DateTime($row['created_at']);
+ $dateFormatted = $date->format('d.m.y H:i');
+ 
+ echo "<div style=\"border: 1px solid #000000;\">
+ <div style=\"border-bottom:1px solid #000000;  padding: 5px; \">$dateFormatted von <a href=\"mailto:$email\">$name</a></div>
+ <div style=\"padding: 5px;\">$text</div>
+ </div><br>";
+ 
+}
+ 
+//Berechne die Anzahl der Seiten:
+$anzahl_seiten = ceil($anzahl_eintrage / floatval($beitraege_pro_seite));
+ 
+//Ausgabe der Seitenlinks:
+echo "<div align=\"center\">";
+echo "<b>Seite:</b> ";
+ 
+ 
+//Ausgabe der Links zu den verschiedenen Seiten
+for($a=1; $a <= $anzahl_seiten; $a++) {
+ //Wenn der User sich auf dieser Seite befindet, keinen Link ausgeben
+ if($seite == $a){
+ echo " <b>$a</b> ";
+ } else { //Aus dieser Seite ist der User nicht, also einen Link ausgeben
+ echo " <a href=\"?seite=$a\">$a</a> ";
+ }
+}
+echo "</div>";
+ 
+?>
+</body>
+</html>
