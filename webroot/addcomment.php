dec3f261f6e1043a5f8654ae77651a2f6737deff
Daniel Kaiser <Daniel.Kaiser@empalis.com>
2021-01-22 15:37:03 +0100

    ss

---
+++ webroot/addcomment.php
@@ -0,0 +1,23 @@
+<?php
+	$host="mysql"; //Add your SQL Server host here
+	$user="cakephp"; //SQL Username
+	$pass="pass4root"; //SQL Password
+	$dbname="default"; //SQL Database Name
+	$con=mysqli_connect($host,$user,$pass,$dbname);
+	if (mysqli_connect_errno($con))
+	{
+		echo "<h1>Failed to connect to MySQL: " . mysqli_connect_error() ."</h1>";
+	}
+	$name=$_POST['name'];
+	$email=$_POST['email'];
+	$message=$_POST['message'];
+	$sql="INSERT INTO guestbook(name,email,message) VALUES('$name','$email','$message')";
+	if (!mysqli_query($con,$sql))
+	{
+		die('Error: ' . mysqli_error($con));
+	}
+	else
+		echo "Values Stored in our Database!";
+	mysqli_close($con);
+?>
+ 
