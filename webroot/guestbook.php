5b3c06a8212d1db0dbb8bd32a29190410a6ac622
Daniel Kaiser <Daniel.Kaiser@empalis.com>
2021-04-12 13:30:49 +0200

    sdsd

--- webroot/guestbook.php
+++ webroot/guestbook.php
@@ -11,9 +11,9 @@
 	$result = mysqli_query($con,"SELECT name,message FROM guestbook");
 	while($row = mysqli_fetch_array($result))
 	{ ?>
-	<div style="width: 500px; background-color: #FF3300; color: white; border: 10px solid #000;"> 
-		<h3>Name: <?php echo $row['name']; ?></h3>
-		<p>Message:</p> <?php echo $row['message']; ?> <br />
+	<div style="width: 50px; background-color: #FF3300; color: white; border: 10px solid #000;"> 
+		<h4>Name: <?php echo $row['name']; ?></h3>
+		<p>Message:</p> <?php echo $row['message']; ?><br />
 	</div>
 	<?php } 
 		mysqli_close($con);
